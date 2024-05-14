06827df43e46f582e022be5d4c5459e0282c2665
SeelyksDev <kevincordier.dev@gmail.com>
2024-05-14 18:52:24 +0200

    Ajout du manifest.json lié à une erreur de la console

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -9,11 +9,11 @@ const Info = () => {
                         <p className="info__location">Île-de-France</p>
                     </li>
                     <li className="info__project info__bubble">
-                        <i class="fa-solid fa-check"></i>
+                        <i className="fa-solid fa-check"></i>
                         <p>Nombre de projets : 12</p>
                     </li>
                     <li className="info__email info__bubble">
-                        <i class="fa-solid fa-envelope"></i>
+                        <i className="fa-solid fa-envelope"></i>
                         <a
                             className="info__mail"
                             href="mailto:kevincordier.dev@gmail.com"
